c0a8ac0ad158a975dcf173990adc761d7b3d3dc3
Michael Brown <michaelde.brown@mail.utoronto.ca>
2019-02-26 14:49:12 -0500

    moving away from printing asap, turning input into data then formatting

--- format_errno.c
+++ format_errno.c
@@ -4,26 +4,24 @@
 
 #define ERRLEN 256
 
-// constantly reads lines from stdin until an EOF
-// assumes input is from errno -l
-// use would be `errno -l | ./format_errno`
-// finds the indexes of the first 2 spaces
-// this should split the line into 3 parts, the error macro name,
-// the macro's value, and the description of the value
-// this program will split the parts into 3 individual strings
-// the strings are reconstructed into their original order, with tabs placed
-// between the strings to keep everything spaced visible
+typedef struct Error { // all fields are on the heap
+  char *name;
+  char *number;
+  char *description;
+  Error *next;
+}
+
 int main() {
+
+  int maxname = 0;
+  int maxnum = 0;
+  int maxdesc = 0;
+
+  Error *root;
 
   char errline[ERRLEN];
   while (fgets(errline, ERRLEN, stdin) != NULL) {
-    char errname[ERRLEN];
-    char errnum[ERRLEN];
-    char errdesc[ERRLEN];
-    char errfin[ERRLEN];
-    int linelen;
-
-    linelen = strlen(errline);
+    int linelen = strlen(errline);
 
     int findex = -1;
     int sindex = -1;
@@ -39,26 +37,49 @@ int main() {
       }
     }
 
+    char errname[ERRLEN];
     strncpy(errname, &errline[0], findex);
     errname[findex] = '\0';
+    int fnamelen = strlen(errname);
 
+    if (fnamelen > maxname) maxname = fnamelen;
+
+    char errnum[ERRLEN];
     int numlen = (sindex - findex) - 1; // the amount of characters between
     strncpy(errnum, &errline[findex + 1], numlen);
     errnum[numlen] = '\0';
+    int fnumlen = strlen(errnum);
 
+    if (fnumlen > maxnum) maxnum = fnumlen;
+
+    char errdesc[ERRLEN];
     int desclen = (linelen - 1) - sindex;
     strncpy(errdesc, &errline[sindex + 1], desclen);
     errdesc[desclen] = '\0';
+    int fdesclen = strlen(errdesc);
 
-    strncpy(errfin, errname, findex + 1);
-    strncat(errfin, "\t\t", 2);
-    strncat(errfin, errnum, numlen);
-    strncat(errfin, "\t\t", 2);
-    strncat(errfin, errdesc, desclen);
+    if (fdesclen > maxdesc) maxdesc = fdesclen;
 
-    //printf("err:%s,%s,%s", errname, errnum, errdesc);
-    printf("%s", errfin);
+    // make new error struct, append it to root list;
+    char *nname = malloc(sizeof(char) * fnamelen);
+    char *nnum = malloc(sizeof(char) * fnumlen);
+    char *ndesc = malloc(sizeof(char) * fdesclen);
   }
+
+  return 0;
+}
+
+Error new(char *name, int namelen, char *num, int numlen, char *desc, int desclen) {
+
+}
+
+int append(Error new, Error *root) {
+  while (root->next != NULL) {
+    root = root->next;
+  }
+
+  root->next = malloc(sizeof(struct Error));
+  *(root->next) = new;
 
   return 0;
 }
